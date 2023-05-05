31b3e0199d0d862585a5e9aef93455572b2c6ee7
Wahyu Eko <wahyu.ehs@gmail.com>
2023-05-05 07:40:27 +0700

    ok

--- nodeapi/src/middleware/fireabase-auth.ts
+++ nodeapi/src/middleware/fireabase-auth.ts
@@ -7,7 +7,7 @@ import * as admin from 'firebase-admin';
 
 admin.initializeApp({
   credential: admin.credential.cert({
-    projectId: process.env.FIREBASE_PROJECT_ID || 'test-one-eb77e',
+    projectId: process.env.FIREBASE_PROJECT_ID,
     clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
     privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
   }),
